6886fef9d395a4aaf4f1e849ed5abb451d76adb2
nayoung3669 <skdud3669@gmail.com>
2023-06-26 12:50:29 +0900

    basic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { INCREASE, DECREASE } from "./redux/modules/counter";
 
 function App() {
   //store에 접근해 counter의 값을 읽어오기
@@ -7,9 +8,17 @@ function App() {
     return state;
   });
 
+  const dispatch = useDispatch();
+
   console.log(data);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <button onClick={() => dispatch({ type: INCREASE })}>z+</button>
+      <button onClick={() => dispatch({ type: DECREASE })}>-</button>
+      현재 카운트: {data.counter.number}
+    </div>
+  );
 }
 
 export default App;
--- src/redux/modules/counter.js
+++ src/redux/modules/counter.js
@@ -1,3 +1,6 @@
+export const INCREASE = "INCREASE";
+export const DECREASE = "DECREASE";
+
 //초기 상태 값
 
 const initialState = {
@@ -7,6 +10,14 @@ const initialState = {
 //리듀서: state를 action의 type에 따라서 변경하는 함수
 const counter = (state = initialState, action) => {
   switch (action.type) {
+    case "counter/INCREASE":
+      return {
+        number: state.number + 1,
+      };
+    case "counter/DECREASE":
+      return {
+        number: state.number - 1,
+      };
     default:
       return state;
   }
