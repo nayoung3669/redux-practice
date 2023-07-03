9ae2737a86c9bde6d4c01ae0a59dbcf99c11ab68
nayoung3669 <skdud3669@gmail.com>
2023-07-03 15:34:10 +0900

    toolkit 변경

--- go-to-redux-practice/redux-counter-basic/src/redux/config/configStore.jsx
+++ go-to-redux-practice/redux-counter-basic/src/redux/config/configStore.jsx
@@ -1,11 +1,9 @@
-import { createStore } from "redux";
-import { combineReducers } from "redux";
 import counter from "../modules/counter";
+import { configureStore } from "@reduxjs/toolkit";
 
 //app 전체에서 counter라는 reducer를 쓸 수 있음
-const rootReducer = combineReducers({
-  counter,
+const store = configureStore({
+  reducer: counter,
 });
-const store = createStore(rootReducer);
 
 export default store;
--- go-to-redux-practice/redux-counter-basic/src/redux/modules/counter.js
+++ go-to-redux-practice/redux-counter-basic/src/redux/modules/counter.js
@@ -1,40 +1,21 @@
-const PLUS_N = "PLUS_N";
-const MINUS_N = "MINUS_N";
+import { createSlice } from "@reduxjs/toolkit";
 
-//action creator functions
-export const plusN = (value) => {
-  return {
-    type: PLUS_N,
-    payload: value,
-  };
-};
-
-export const minusN = (value) => {
-  return {
-    type: MINUS_N,
-    payload: value,
-  };
-};
-
-//초기 상태 값
 const initialState = {
   number: 0,
 };
 
-//리듀서: state를 action의 type에 따라서 변경하는 함수
-const counter = (state = initialState, action) => {
-  switch (action.type) {
-    case PLUS_N:
-      return {
-        number: state.number + action.payload,
-      };
-    case MINUS_N:
-      return {
-        number: state.number - action.payload,
-      };
-    default:
-      return state;
-  }
-};
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    plusN: (state, action) => {
+      state.number = state.number + action.payload;
+    },
+    minusN: (state, action) => {
+      state.number = state.number - action.payload;
+    },
+  },
+});
 
-export default counter;
+export default counterSlice.reducer;
+export const { plusN, minusN } = counterSlice.actions;
